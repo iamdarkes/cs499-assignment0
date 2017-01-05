0cdcac16168d434c3e8819de107b8278f9b09ad7
Victor Darkes <josephdarkes@gmail.com>
2017-01-04 20:59:09 -0800

    Created four separate Hello World applications done in - Java, HTML, Node.js, and Python

---
+++ JavaHelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Assignment: Assignment 0
+ * Instructor: Yu Sun
+ * Course: CS 499
+ * Creator: Victor Darkes
+ * Date: 1/4/2017
+ * Description: This is a Hello World program done in Java.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World in Java.");
+    }
+}
---
+++ NodeHelloWorld/helloworld.js
@@ -0,0 +1,10 @@
+/**
+ * Assignment: Assignment 0
+ * Instructor: Yu Sun
+ * Course: CS 499
+ * Creator: Victor Darkes
+ * Date: 1/4/2017
+ * Description: This is a Hello World program done in Node.js.
+ */
+
+console.log("Hello World in Javascript.");
